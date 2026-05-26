5b5fd67d50420967143990ba97f047dc6204188f
SHelShol <Parasytik2007@gmail.com>
2026-05-26 10:48:07 +0500

    add recommendations module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ScheduleModule } from '@nestjs/schedule';
 import { RoomsModule } from './rooms/rooms.module';
 import { ServiceTypesModule } from './service-types/service-types.module';
 import { AnalyticsModule } from './analytics/analytics.module';
+import { RecommendationsModule } from './recommendations/recommendations.module';
 
 @Module({
-  imports: [RoomsModule, ServiceTypesModule, AnalyticsModule],
+  imports: [
+    ScheduleModule.forRoot(),
+    RoomsModule,
+    ServiceTypesModule,
+    AnalyticsModule,
+    RecommendationsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/recommendations/recommendations.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Patch, Param } from '@nestjs/common';
+import { RecommendationsService } from './recommendations.service';
+
+@Controller('recommendations')
+export class RecommendationsController {
+  constructor(private readonly recommendationsService: RecommendationsService) {}
+
+  @Get()
+  findAll() {
+    return this.recommendationsService.findAll();
+  }
+
+  @Patch(':id/resolve')
+  resolve(@Param('id') id: string) {
+    return this.recommendationsService.resolve(Number(id));
+  }
+}
